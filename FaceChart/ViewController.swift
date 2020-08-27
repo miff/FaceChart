ac4976ce0d6d854ee988c70ad35ffaa9ad2a9f44
Milan Djordjevic <miffmedia@gmail.com>
2020-08-27 20:47:23 +0200

    Initial Commit

---
+++ FaceChart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceChart
+//
+//  Created by Milan Djordjevic on 8/27/20.
+//  Copyright © 2020 miff.me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
